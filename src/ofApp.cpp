d33a209f786a1422f837857b79004f739807f7ad
Karin <karin_lr@hotmail.com>
2017-01-30 12:29:32 +0000

    random color every sec

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -5,32 +5,48 @@ void ofApp::setup(){
     
     ofBackground(60, 179, 113);
     
+    randR = ofRandom(255);
+    randG = ofRandom(255);
+    randB = ofRandom(255);
+    
 
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+    
+    if (ofGetFrameNum() % 60 == 0){
+        randR = ofRandom(255);
+        randG = ofRandom(255);
+        randB = ofRandom(255);
+    }
 
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    /*randR = ofRandom(255);
-    randG = ofRandom(255);
-    randB = ofRandom(255);
     
-    ofBackground(randR, randG, randB);
+    //ofBackground(randR, randG, randB);
     
     string info = "fps: " + ofToString(ofGetFrameRate(),2);
-    string rgbValues = "RGB (" + ofToString(randR) + ", " + ofToString(randG) + ", " + ofToString(randB) + ")";
+    //string rgbValues = "RGB (" + ofToString(randR) + ", " + ofToString(randG) + ", " + ofToString(randB) + ")";
+    ofSetColor(0, 0, 0);
     ofDrawBitmapString(info, 100, 100);
-    ofDrawBitmapString(rgbValues, 100, 110); */
+    //ofDrawBitmapString(rgbValues, 100, 110);
     
-    ofSetColor(248, 131, 121);
+    // CIRCLE
+    //ofSetColor(248, 131, 121);
+    ofSetColor(randR, randG, randB);
     ofDrawCircle(ofGetWidth()/3, ofGetHeight()/2, 100);
-    ofSetColor(248, 121, 175);
+    
+    // RECTANGLE
+    //ofSetColor(248, 121, 175);
+    ofSetColor(randR, randG, randB);
     ofDrawRectangle(500, 500, 200, 200);
-    ofSetColor(121, 175, 248);
+    
+    // TRIANGLE
+    //ofSetColor(121, 175, 248);
+    ofSetColor(randR, randG, randB);
     ofDrawTriangle(200, 200, 250, 250, 150, 250);
     
     
